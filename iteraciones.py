94655081bb263ec90e92e00b78f468ef9d9db30a
Juan Diego S <juandiegoo_93@hotmail.com>
2022-12-08 17:19:41 -0500

    2do programa

--- iteraciones.py
+++ iteraciones.py
@@ -1,5 +1,10 @@
-contador = 0
+contador_externo = 0
+contador_interno = 0
 
-while contador < 10:
-    print(contador)
-    contador += 1 #contador mas uno
+while contador_externo < 5:
+    while contador_interno < 6:
+        print(contador_externo, contador_interno)
+        contador_interno += 1
+
+    contador_externo += 1
+    contador_interno = 0
