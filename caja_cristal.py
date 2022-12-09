f57092481c69e72835700878590b8542ceb40cf3
Juan Diego S <juandiegoo_93@hotmail.com>
2022-12-08 23:12:47 -0500

    caja de cristal

--- caja_cristal.py
+++ caja_cristal.py
@@ -9,14 +9,12 @@ def es_mayor_de_edad(edad):
 class PruebaDeCristalTest(unittest.TestCase):
     def test_es_mayor_de_edad(self):
         edad = 20
-
         resultado = es_mayor_de_edad(edad)
 
         self.assertEqual(resultado, True)
 
     def test_es_menor_de_edad(self):
         edad = 15
-
         resultado = es_mayor_de_edad(edad)
 
         self.assertEqual(resultado, False)
